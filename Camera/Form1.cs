1217f02a8bf9f2c197c9f50edd18f74a04719364
Mhage <masahage@soleil.ocn.ne.jp>
2024-01-03 11:13:22 +0900

    Cameraプロジェクト新規作成

---
+++ Camera/Form1.cs
@@ -0,0 +1,136 @@
+using System.Numerics;
+
+namespace Camera
+{
+    public partial class Form1 : Form
+    {
+        float thetaX = 0.2f;                                               // X軸回転角度
+        float thetaY = -0.4f;                                               // Y軸回転角度
+        float thetaZ = 0;                                                           // Z軸回転角度
+        float scale = 55;                                                          // 拡大係数
+        const float light_thetaX = 1.5f;
+        Vector3 light = new(0, MathF.Cos(light_thetaX), MathF.Sin(light_thetaX));   // 光の方向ベクトル
+        Vector3 offset = new(300f, 450f, 0);                                        // 平行移動の量
+        //Model model = new("../../../Model/Shanghai.csv");
+        Model model = new("../../../Model/Shanghai.pmx", true);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawPolygons(Vertex[] vertices, Bitmap screen, float[,] zBuffer, bool textureMapping, bool gourauShading)
+        {
+            for (int m = 0; m < model.faces.Length; ++m)
+            {
+                var color = model.faces[m].material.color;
+                var texture = model.faces[m].material.texture;
+                var length = model.faces[m].vertexNumbers.Length;                          // ポリゴン一つごとの頂点の数。MMDの場合は常に3。
+                var vs = new Vertex[length];
+                for (int i = 0; i < length; ++i)
+                {
+                    var v = vertices[model.faces[m].vertexNumbers[i]];
+                    vs[i] = new Vertex(v.pos, v.pseudoNormal, v.uv);
+                }
+                vs = vs.OrderBy(v => v.pos.Y).ToArray();
+                if (MathF.Abs(vs[0].pos.Y - vs[2].pos.Y) < 0.1f) continue;                  // 三角形じゃない
+                var normal = MakePositiveNormal(GetNormalOfTrigngle(vs.Select(v => v.pos).ToArray()));
+                float brightness = Clip(0, 1, light.X * normal.X + light.Y * normal.Y + light.Z * normal.Z);
+                for (int y = (int)vs[0].pos.Y; y < vs[2].pos.Y; ++y)                        // 三角形を覆う全ての横線について行う
+                {
+                    if (y < 0 || y >= screen.Height) continue;                              // 画面サイズでクリッピング
+                    int j = (MathF.Abs(vs[0].pos.Y - vs[1].pos.Y) < 0.1f || y >= vs[1].pos.Y) ? 1 : 0;
+                    var x1 = Clip(vs[j].pos.X, vs[j + 1].pos.X, vs[j].pos.X + (y - vs[j].pos.Y) * (vs[j + 1].pos.X - vs[j].pos.X) / (vs[j + 1].pos.Y - vs[j].pos.Y));  // 計算誤差対策
+                    var x2 = Clip(vs[0].pos.X, vs[2].pos.X, vs[0].pos.X + (y - vs[0].pos.Y) * (vs[2].pos.X - vs[0].pos.X) / (vs[2].pos.Y - vs[0].pos.Y));
+                    var z1 = vs[j].pos.Z + (y - vs[j].pos.Y) * (vs[j + 1].pos.Z - vs[j].pos.Z) / (vs[j + 1].pos.Y - vs[j].pos.Y);
+                    var z2 = vs[0].pos.Z + (y - vs[0].pos.Y) * (vs[2].pos.Z - vs[0].pos.Z) / (vs[2].pos.Y - vs[0].pos.Y);
+                    var pn1 = vs[j].pseudoNormal + (y - vs[j].pos.Y) * (vs[j + 1].pseudoNormal - vs[j].pseudoNormal) / (vs[j + 1].pos.Y - vs[j].pos.Y);
+                    var pn2 = vs[0].pseudoNormal + (y - vs[0].pos.Y) * (vs[2].pseudoNormal - vs[0].pseudoNormal) / (vs[2].pos.Y - vs[0].pos.Y);
+                    var uv1 = vs[j].uv + (y - vs[j].pos.Y) * (vs[j + 1].uv - vs[j].uv) / (vs[j + 1].pos.Y - vs[j].pos.Y);
+                    var uv2 = vs[0].uv + (y - vs[0].pos.Y) * (vs[2].uv - vs[0].uv) / (vs[2].pos.Y - vs[0].pos.Y);
+                    for (int x = (int)Math.Min(x1, x2); x <= (int)Math.Max(x1, x2); ++x)
+                    {
+                        if (x < 0 || x >= screen.Width) continue;                           // 画面サイズでクリッピング
+                        var z = x2 == x1 ? z1 : z1 + (x - x1) * (z2 - z1) / (x2 - x1);      // Z座標を計算
+                        if (z > zBuffer[x, y]) continue;                                    // 今回のものが奥にあれば何もしない
+                        if (textureMapping)
+                        {
+                            var uv = x2 == x1 ? uv1 : uv1 + (x - x1) * (uv2 - uv1) / (x2 - x1);
+                            uv.X = Clip(0, 1, uv.X);                                        // 計算誤差対策
+                            uv.Y = Clip(0, 1, uv.Y);
+                            if (texture != null) color = texture.GetPixel((int)((texture.Width - 1) * uv.X), (int)((texture.Height - 1) * uv.Y));
+                        }
+                        var brightenedColor = Color.FromArgb(color.A, (int)(color.R * brightness), (int)(color.G * brightness), (int)(color.B * brightness));
+                        if (gourauShading)
+                        {
+                            var n = x2 == x1 ? pn1 : pn1 + (x - x1) * (pn2 - pn1) / (x2 - x1);
+                            n = MakePositiveNormal(n);
+                            brightness = Clip(0, 1, light.X * n.X + light.Y * n.Y + light.Z * n.Z);
+                            brightenedColor = Color.FromArgb(color.A, (int)(color.R * brightness), (int)(color.G * brightness), (int)(color.B * brightness));
+                        }
+                        if (brightenedColor.A == 0) continue;                                 // 透明な色は塗らず、Zバッファを更新しない
+                        screen.SetPixel(x, y, brightenedColor);
+                        zBuffer[x, y] = z;                                                    // 奥行の値を更新
+                    }
+                }
+            }
+        }
+
+        float Clip(float min, float max, float value)
+        {
+            return MathF.Min(MathF.Max(min, max), MathF.Max(MathF.Min(min, max), value));
+        }
+
+        /// <summary>
+        /// 法線を視点(Z方向)から見て正の方向になるようにする
+        /// </summary>
+        Vector3 MakePositiveNormal(Vector3 normal)
+        {
+            return normal.Z > 0 ? normal : -normal;
+        }
+
+        Vertex[] TransformVertices(Vertex[] vertices, Vector3 offset)
+        {
+            var vs = new Vertex[vertices.Length];  // 移動後の頂点
+            for (int i = 0; i < vs.Length; ++i)
+            {
+                Vector3 pos = new(vertices[i].pos.X, -vertices[i].pos.Y, vertices[i].pos.Z);   // MMDモデルはY軸が反転している
+                pos = new(pos.Z * MathF.Sin(thetaY) + pos.X * MathF.Cos(thetaY), pos.Y, pos.Z * MathF.Cos(thetaY) - pos.X * MathF.Sin(thetaY));// Y軸回転
+                pos = new(pos.X * MathF.Cos(thetaZ) - pos.Y * MathF.Sin(thetaZ), pos.X * MathF.Sin(thetaZ) + pos.Y * MathF.Cos(thetaZ), pos.Z);// Z軸回転
+                pos = new(pos.X, pos.Y * MathF.Cos(thetaX) - pos.Z * MathF.Sin(thetaX), pos.Y * MathF.Sin(thetaX) + pos.Z * MathF.Cos(thetaX));// X軸回転
+                pos = new(scale * pos.X, scale * pos.Y, scale * pos.Z);                    // 拡大
+                pos = new(pos.X + offset.X, pos.Y + offset.Y, pos.Z + offset.Z);           // 平行移動
+                Vector3 pn = new(vertices[i].pseudoNormal.X, -vertices[i].pseudoNormal.Y, vertices[i].pseudoNormal.Z);     // MMDモデルはY軸が反転している
+                pn = new(pn.Z * MathF.Sin(thetaY) + pn.X * MathF.Cos(thetaY), pn.Y, pn.Z * MathF.Cos(thetaY) - pn.X * MathF.Sin(thetaY));// Y軸回転
+                pn = new(pn.X * MathF.Cos(thetaZ) - pn.Y * MathF.Sin(thetaZ), pn.X * MathF.Sin(thetaZ) + pn.Y * MathF.Cos(thetaZ), pn.Z);// Z軸回転
+                pn = new(pn.X, pn.Y * MathF.Cos(thetaX) - pn.Z * MathF.Sin(thetaX), pn.Y * MathF.Sin(thetaX) + pn.Z * MathF.Cos(thetaX));// X軸回転
+                vs[i] = new Vertex(pos, pn, vertices[i].uv);
+            }
+            return vs;
+        }
+
+        Vector3 GetNormalOfTrigngle(Vector3[] vertices)
+        {
+            Vector3 a = new(vertices[1].X - vertices[0].X, vertices[1].Y - vertices[0].Y, vertices[1].Z - vertices[0].Z);
+            Vector3 b = new(vertices[2].X - vertices[0].X, vertices[2].Y - vertices[0].Y, vertices[2].Z - vertices[0].Z);
+            float n = MathF.Sqrt(MathF.Pow(a.Y * b.Z - a.Z * b.Y, 2) + MathF.Pow(a.Z * b.X - a.X * b.Z, 2) + MathF.Pow(a.X * b.Y - a.Y * b.X, 2));
+            return new((a.Y * b.Z - a.Z * b.Y) / n, (a.Z * b.X - a.X * b.Z) / n, (a.X * b.Y - a.Y * b.X) / n); // 法線ベクトル
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            thetaY += 0.1f; // 一定時間ごとにY軸回転角度を増やす
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var screen = new Bitmap(e.ClipRectangle.Width, e.ClipRectangle.Height);     // 画面サイズの画像データを作る
+            var zBuffers = new float[e.ClipRectangle.Width, e.ClipRectangle.Height];    // ピクセルの奥行きの値
+            for (int i = 0; i < zBuffers.GetLength(0); i++) for (int j = 0; j < zBuffers.GetLength(1); j++) zBuffers[i, j] = float.MaxValue; // 初期値
+            var transformedVertices = TransformVertices(model.vertices, offset);        // 頂点の平行移動と回転
+            DrawPolygons(transformedVertices, screen, zBuffers, true, true);            // テクスチャマッピングと、グーローシェーディングは引数でON/OFF切り替え可能
+            e.Graphics.DrawImage(screen, 0, 0);                                         // 画像データを画面に表示する
+        }
+    }
+}
---
+++ Camera/Model.cs
@@ -0,0 +1,274 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+namespace Camera
+{
+    /// <summary>
+    /// 各種情報を含む頂点
+    /// </summary>
+    internal class Vertex
+    {
+        public Vector3 pos;             // 位置
+        public Vector3 pseudoNormal;    // 疑似法線ベクトル
+        public Vector2 uv;              // UV座標
+
+        public Vertex() { }
+
+        public Vertex(Vector3 pos, Vector3? pseudoNormal, Vector2? uv)
+        {
+            this.pos = pos;
+            if (pseudoNormal != null) this.pseudoNormal = (Vector3)pseudoNormal;
+            if (uv != null) this.uv = (Vector2)uv;
+        }
+    }
+
+    /// <summary>
+    /// 面。頂点番号の組み合わせと材質を持つ。MMDではFaceと呼ぶ。
+    /// </summary>
+    internal class Face
+    {
+        public int[] vertexNumbers;
+        public Material material;
+
+        public Face() { }
+
+        public Face(int[] vertexNumbers, Material material)
+        {
+            this.vertexNumbers = vertexNumbers;
+            this.material = material;
+        }
+    }
+
+    /// <summary>
+    /// 材質。色とテクスチャを持つ。
+    /// </summary>
+    internal class Material
+    {
+        public String name;
+        public Color color;
+        public Bitmap? texture;
+
+        public Material() { }
+
+        public Material(String name, Color color, Bitmap? texture)
+        {
+            this.name = name;
+            this.color = color;
+            this.texture = texture;
+        }
+    }
+
+    internal class Model
+    {
+        public Vertex[] vertices;
+        public Material[] materials;
+        public Face[] faces;
+
+        /// <param name="filename">MMDモデル用のPMXファイルをPMXエディターで開いてCSV出力したファイルの名前</param>
+        public Model(String filename)
+        {
+            // C#はデフォルトでShift_JISに非対応。NuGetパッケージでSystem.Text.Encoding.CodePagesをインストールし、以下のメソッド実行でShift_JISを使用可能にする。
+            // 日本のWindows環境のCSVはShift_JISがデフォルト。PMX EditorのCSV出力もShift_JISが使用されている。
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var lines = File.ReadAllLines(filename, System.Text.Encoding.GetEncoding("Shift_JIS"));
+            var data = new String[lines.Length][];
+            for (int i = 0; i < lines.Length; i++) data[i] = lines[i].Split(',');
+            var vertexList = new List<Vertex>();
+            var faceList = new List<Face>();
+            var materialList = new List<Material>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (data[i][0] == "Vertex")
+                {
+                    var pos = new float[3];
+                    for (int j = 0; j < pos.Length; ++j) pos[j] = Convert.ToSingle(data[i][2 + j]);
+                    var pseudoNormal = new float[3];
+                    for (int j = 0; j < pseudoNormal.Length; ++j) pseudoNormal[j] = Convert.ToSingle(data[i][5 + j]);
+                    var uv = new float[2];
+                    for (int j = 0; j < uv.Length; ++j) uv[j] = Convert.ToSingle(data[i][9 + j]);
+                    vertexList.Add(new(new Vector3(pos), new Vector3(pseudoNormal), new Vector2(uv)));
+                }
+                if (data[i][0] == "Material")
+                {
+                    var rgba = new int[4];
+                    for (int j = 0; j < rgba.Length; ++j) rgba[j] = (int)(255 * Convert.ToSingle(data[i][3 + j]));
+                    var textureFilename = data[i][26].Trim('\"');   // PMX EditorのCSV出力は文字列がダブルクォーテーションで囲われている。Excelなどで編集して保存するとダブルクォーテーションがなくなる。
+                    var texture = textureFilename == "" ? null : new Bitmap(Path.GetDirectoryName(filename) + "/" + textureFilename);  // テクスチャファイルはbmp, png, jpgに対応。tgaは非対応。モデルのファイルと同じフォルダにあるとする
+                    materialList.Add(new(data[i][1], Color.FromArgb(rgba[3], rgba[0], rgba[1], rgba[2]), texture));
+                }
+            }
+            vertices = vertexList.ToArray();
+            materials = materialList.ToArray();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (data[i][0] == "Face")
+                {
+                    var vertexNumbers = new int[3];
+                    for (int j = 0; j < vertexNumbers.Length; ++j) vertexNumbers[j] = Convert.ToInt32(data[i][3 + j]);
+                    var material = materials.First(x => x.name == data[i][1]);
+                    faceList.Add(new Face(vertexNumbers, material));
+                }
+            }
+            faces = faceList.ToArray();
+        }
+
+        public Model(String filename, bool pmx)
+        {
+            if (!pmx) return;
+            using (var reader = new BinaryReader(new FileStream(filename, FileMode.Open))) // BinaryReaderクラスとPMXデータフォーマットは共にリトルエンディアン
+            {
+                // ヘッダ
+                var headerStr = Encoding.ASCII.GetString(reader.ReadBytes(4));
+                Debug.WriteLine("headerStr : " + headerStr);
+                var ver = reader.ReadSingle();
+                Debug.WriteLine("ver : " + ver);        // 2.0 または 2.1 でなければならない
+                var headerInfoSize = reader.ReadByte();
+                Debug.WriteLine("headerInfoSize : " + headerInfoSize); // 8でなければならない
+                var encodeTypeNumber = reader.ReadByte();
+                var encoding = encodeTypeNumber == 0 ? Encoding.Unicode : Encoding.UTF8;
+                Debug.WriteLine("encoding : " + encoding);
+                var numberOfAdditionalUV = reader.ReadByte();
+                Debug.WriteLine("numberOfAdditionalUV : " + numberOfAdditionalUV);
+                var vertexIndexSize = reader.ReadByte();
+                Debug.WriteLine("vertexIndexSize : " + vertexIndexSize);
+                var textureIndexSize = reader.ReadByte();
+                Debug.WriteLine("textureIndexSize : " + textureIndexSize);
+                var materialIndexSize = reader.ReadByte();
+                Debug.WriteLine("materialIndexSize : " + materialIndexSize);
+                var boneIndexSize = reader.ReadByte();
+                Debug.WriteLine("boneIndexSize : " + boneIndexSize);
+                var morphIndexSize = reader.ReadByte();
+                Debug.WriteLine("morphIndexSize : " + morphIndexSize);
+                var rigidBodyIndexSize = reader.ReadByte();
+                Debug.WriteLine("rigidBodyIndexSize : " + rigidBodyIndexSize);
+                var sizeOfModelName = reader.ReadInt32();
+                var modelName = encoding.GetString(reader.ReadBytes(sizeOfModelName));
+                Debug.WriteLine("modelName : " + modelName);
+                var sizeOfEnglishModelName = reader.ReadInt32();
+                var EnglishModelName = encoding.GetString(reader.ReadBytes(sizeOfEnglishModelName));
+                Debug.WriteLine("EnglishModelName : " + EnglishModelName);
+                var sizeOfComment = reader.ReadInt32();
+                var comment = encoding.GetString(reader.ReadBytes(sizeOfComment));
+                Debug.WriteLine("comment : " + comment);
+                var sizeOfEnglishComment = reader.ReadInt32();
+                var EnglishComment = encoding.GetString(reader.ReadBytes(sizeOfEnglishComment));
+                Debug.WriteLine("EnglishComment : " + EnglishComment);
+
+                // 頂点
+                var numberOfVertex = reader.ReadInt32();
+                Debug.WriteLine("sizeOfVertex : " + numberOfVertex);
+                vertices = new Vertex[numberOfVertex];
+                for (int i = 0; i < numberOfVertex; i++)
+                {
+                    vertices[i] = new Vertex();
+                    vertices[i].pos.X = reader.ReadSingle();
+                    vertices[i].pos.Y = reader.ReadSingle();
+                    vertices[i].pos.Z = reader.ReadSingle();
+                    vertices[i].pseudoNormal.X = reader.ReadSingle();
+                    vertices[i].pseudoNormal.Y = reader.ReadSingle();
+                    vertices[i].pseudoNormal.Z = reader.ReadSingle();
+                    vertices[i].uv.X = reader.ReadSingle();
+                    vertices[i].uv.Y = reader.ReadSingle();
+                    reader.ReadBytes(16 * numberOfAdditionalUV);    // 使用しない
+                    var weightTransformType = reader.ReadByte();
+                    switch (weightTransformType)
+                    {
+                        // 全て使用しない
+                        case 0: // BDEF1
+                            reader.ReadBytes(boneIndexSize);
+                            break;
+                        case 1: // BDEF2
+                            reader.ReadBytes(2 * boneIndexSize + 4);
+                            break;
+                        case 2: // BDEF4
+                            reader.ReadBytes(4 * boneIndexSize + 4 * 4);
+                            break;
+                        case 3: // SDEF
+                            reader.ReadBytes(2 * boneIndexSize + 4 + 3 * 12);
+                            break;
+                    }
+                    reader.ReadSingle();    // 使用しない
+                }
+                Debug.WriteLine("Last Vertex pos X: " + vertices[numberOfVertex - 1].pos.X);
+
+                // 面
+                var numberOfFace = reader.ReadInt32() / 3;  // 注意：PMX仕様.txtの「面数」は面の数ではなく面を構成している頂点の数を表す。そのため3で割る必要がある。
+                Debug.WriteLine("numberOfFace : " + numberOfFace);
+                faces = new Face[numberOfFace];
+                for (int i = 0; i < numberOfFace; i++)
+                {
+                    faces[i] = new Face();
+                    faces[i].vertexNumbers = new int[3];
+                    switch (vertexIndexSize)
+                    {
+                        case 1:
+                            for (int j = 0; j < 3; ++j) faces[i].vertexNumbers[j] = reader.ReadByte();  // 符号無し 8bit
+                            break;
+                        case 2:
+                            for (int j = 0; j < 3; ++j) faces[i].vertexNumbers[j] = reader.ReadUInt16();// 符号無し 16bit
+                            break;
+                        case 4:
+                            for (int j = 0; j < 3; ++j) faces[i].vertexNumbers[j] = reader.ReadInt32(); // 符号あり 32bit
+                            break;
+                    }
+                }
+
+                // テクスチャ
+                var numberOfTexture = reader.ReadInt32();
+                Debug.WriteLine("numberOfTexture : " + numberOfTexture);
+                var texturePaths = new String[numberOfTexture];
+                for (int i = 0; i < numberOfTexture; i++)
+                {
+                    var sizeOfTexturePath = reader.ReadInt32();
+                    texturePaths[i] = encoding.GetString(reader.ReadBytes(sizeOfTexturePath));
+                }
+
+                // 材質
+                var numberOfMaterial = reader.ReadInt32();
+                Debug.WriteLine("numberOfMaterial : " + numberOfMaterial);
+                materials = new Material[numberOfMaterial];
+                int faceIndex = 0;
+                for (int i = 0; i < numberOfMaterial; i++)
+                {
+                    materials[i] = new Material();
+                    var sizeOfMaterialName = reader.ReadInt32();
+                    materials[i].name = encoding.GetString(reader.ReadBytes(sizeOfMaterialName));
+                    var sizeOfEnglishMaterialName = reader.ReadInt32();
+                    encoding.GetString(reader.ReadBytes(sizeOfEnglishMaterialName));    // 使用しない
+                    var rgba = new int[4];
+                    for (int j = 0; j < rgba.Length; ++j) rgba[j] = (int)(255 * reader.ReadSingle());
+                    materials[i].color = Color.FromArgb(rgba[3], rgba[0], rgba[1], rgba[2]);
+                    reader.ReadBytes(12 + 4 + 12 + 1 + 16 + 4);  // 使用しない
+                    int textureIndex = 0;
+                    switch (textureIndexSize)
+                    {
+                        case 1:
+                            textureIndex = reader.ReadSByte(); // 符号あり 8bit
+                            break;
+                        case 2:
+                            textureIndex = reader.ReadInt16(); // 符号あり 16bit
+                            break;
+                        case 4:
+                            textureIndex = reader.ReadInt32(); // 符号あり 32bit
+                            break;
+                    }
+                    materials[i].texture = textureIndex == -1 ? null : new Bitmap(Path.GetDirectoryName(filename) + "/" + texturePaths[textureIndex]);
+                    reader.ReadBytes(textureIndexSize + 1); // 使用しない
+                    var commonToonFlag = reader.ReadByte();
+                    if (commonToonFlag == 0) reader.ReadBytes(textureIndexSize);    // どちらも使用しない
+                    else reader.ReadByte();
+                    var sizeOfMemo = reader.ReadInt32();
+                    reader.ReadBytes(sizeOfMemo);       // 使用しない
+                    var n = reader.ReadInt32() / 3;     // 材質に対応する面数
+                    for (int j = 0; j < n; j++)
+                    {
+                        faces[faceIndex].material = materials[i];
+                        faceIndex++;
+                    }
+                }
+            }
+        }
+    }
+}
